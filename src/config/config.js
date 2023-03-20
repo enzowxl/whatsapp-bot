ee4c1db1debb38604af36bc8e0de5c19f996a5d7
enzowxl <whayyzs13@gmail.com>
2023-03-20 14:43:44 -0300

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+const prefix = "!" // Mude caso quiser
+const number = "" // Mude
+const apiKeyOpenAI = "" // Mude
+const organizationOpenAI = "" // Mude
+const initial = "/" // Não mude
+
+
+
+
+export { 
+
+    prefix,
+    number,
+    apiKeyOpenAI,
+    organizationOpenAI,
+    initial
+
+}
