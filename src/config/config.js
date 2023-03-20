a52060c0d611bee4d28b2b11797588a685b89c3e
enzowxl <whayyzs13@gmail.com>
2023-03-20 15:20:16 -0300

    app

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,19 @@
 const prefix = "!" // Mude caso quiser
-const number = "" // Mude
-const apiKeyOpenAI = "" // Mude
-const organizationOpenAI = "" // Mude
+const number = "5511930688152@c.us" // Mude
+const apiKeyOpenAI = "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" // Mude
+const organizationOpenAI = "org-eOACS68xmx94rM2MYD7ehhhP" // Mude
 const initial = "/" // Não mude
 
 
 
 
-export { 
-
+module.exports [
     prefix,
     number,
     apiKeyOpenAI,
     organizationOpenAI,
     initial
+]
 
-}
+
+
